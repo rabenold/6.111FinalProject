5e727194b3efd3ef9d04367fd1cc932ae203433d
diegorod <diegorod@mit.edu>
2022-11-12 20:47:58 -0500

    completed dither module

--- filters/ditherConv.sv
+++ filters/ditherConv.sv
@@ -25,8 +25,14 @@ module ditherConv #(
 
     logic[10:0] line1_writeOut;
     // logic[10:0] line2_writeOut;
-    xilinx_true_dual_port_read_first_1_clock_ram #(.RAM_WIDTH(WIDTH), .RAM_DEPTH(320)) line1(
-    .addra(hcount_pipe[1]),     // Address bus, width determined from RAM_DEPTH
+
+    logic[2:0][10:0] hcount_pipe;
+    logic [1:0][9:0] vcount_pipe;
+    logic [1:0] valid_pipe;
+    logic [1:0][10:0] pixel_pipe;
+
+    xilinx_true_dual_port_read_first_1_clock_ram #(.RAM_WIDTH(WIDTH), .RAM_DEPTH(320)) line(
+    .addra(hcount_pipe[2]),     // Address bus, width determined from RAM_DEPTH
     .dina(0),       // RAM input data, width determined from RAM_WIDTH
     .clka(clk_in),       // Clock
     .wea(1'b0),         // Write enable
@@ -37,7 +43,7 @@ module ditherConv #(
 
     .addrb(hcount_pipe[0]),     // Address bus, width determined from RAM_DEPTH
     .dinb(bottom_Offsets[0]),       // RAM input data, width determined from RAM_WIDTH
-    .web(1'b1),         // Write enable
+    .web(data_valid_in),         // Write enable
     .enb(1'b1),         // RAM Enable, for additional power savings, disable port when not in use
     .rstb(rst_in),       // Output reset (does not affect memory contents)
     .regceb(1'b1),   // Output register enable
@@ -64,27 +70,45 @@ module ditherConv #(
     // );
    
     // Your code here!
+    logic signed [10:0] final_error;
     logic signed [10:0] errorOffset;
+
     logic signed [10:0] nextOffset;
     logic signed [2:0][10:0] bottom_Offsets;
     logic signed [10:0] current_pixel;
-    logic signed [10:0] final_error;
-    logic signed [10:0] error_input;
 
     assign final_error = errorOffset+nextOffset+current_pixel;
-
-    
+    assign error_Offset = $signed(line1_read);
 
     always_ff @(posedge clk_in) begin
         if(rst_in)begin
-
+            nextOffset <=0;
+            bottom_Offsets <=0;
+            current_pixel <=0;
+            
+            pixel_pipe <=0;
+            hcount_pipe <=0;
+            vcount_pipe <=0;
+            valid_pipe <=0;
         end else if (data_valid_in) begin
+
+            current_pixel<= pixel_pipe[0];
+
+            pixel_pipe <= {data_in,pixel_pipe[1]}
+            hcount_pipe <= {hcount_in, hcount_pipe[1:0]};
+            vcount_pipe <= {vcount_in, vcount_pipe[1]};
+            valid_pipe <= {data_valid_in, valid_pipe[1]};
+
             if(current_pixel+errorOffset+nextOffset < MIDPOINT)begin
+                pixel_out <= 0;
+
                 nextOffset <= (final_error*7)>>>4;
                 bottom_Offsets[0] <= bottom_Offsets[1] + ((final_error*3)>>>4);
                 bottom_Offsets[1] <= bottom_Offsets[2] + ((final_error*5)>>>4);
                 bottom_Offsets[2] <= ((final_error)>>>4);
             end else begin
+                pixel_out <=1;
+
                 nextOffset <= ((final_error-MAX_VAL)*7)>>>4;
                 bottom_Offsets[0] <= bottom_Offsets[1] + (((final_error-MAX_VAL)*3)>>>4);
                 bottom_Offsets[1] <= bottom_Offsets[2] + (((final_error-MAX_VAL)*5)>>>4);
@@ -93,15 +117,9 @@ module ditherConv #(
         end
     end
 
-    assign line_out = {r_out,g_out,b_out};
-    assign data_valid_out = valid_pipe[1];
+    assign data_valid_out = valid_pipe[0];
     assign hcount_out = hcount_pipe[1];
-    assign vcount_out = vcount_pipe[1];
-    // always_ff @(posedge clk_in) begin
-    //   // Make sure to have your output be set with registered logic!
-    //   // Otherwise you'll have timing violations.
-    //   line_out <= {r, g, 1'b0, b};
-    // end
+    assign vcount_out = vcount_pipe[0];
 endmodule
 
 `default_nettype wire
