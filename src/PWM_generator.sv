c20389c226b747fb985fbc6224b79079edc2146a
rabenold <55027363+rabenold@users.noreply.github.com>
2022-12-12 19:59:31 -0500

    PWM Gen to 40hz
    Updating pwm generator to operate at 40hz rather than previous of 10hz

--- src/PWM_generator.sv
+++ src/PWM_generator.sv
@@ -6,19 +6,19 @@
         *   the stepper motors
         *
         *   The module runs off the FPGA's 100 MHz clock.
-        *   The desired PWM frequency is 10 Hz at a 50% duty cycle.  
-        *   To get this, we have a counter that goes up to 5,000,000.
-        *   100 mill / 5 mill = 20, or 10 Hz @ 50% duty cycle.  
+        *   The desired PWM frequency is 40 Hz at a 50% duty cycle.  
+        *   To get this, we use a counter that goes up to 1,250,000.
+        *   100 mill / 1.25 mill = 80, or 40 Hz @ 50% duty cycle.  
         *   
         *   We only want to pulse the motors on the rising edge of the PWM
         *   signal, so as hz_ctr is reset to 0, trigger_step goes high.
         *   Trigger_step is set low if not at a rising edge. 
         *  
         *   Trigger step is shifted into enable step, which runs off the
-        *   100MHz clock, and only goes high on the positive edge of the 10 Hz
+        *   100MHz clock, and only goes high on the positive edge of the 40 Hz
         *   clock. 
         *
-        *   Clk_high indicates a positive edge of the 10Hz signal. Used in
+        *   Clk_high indicates a positive edge of the 40Hz signal. Used in
         *   top_level as a visual confirmation that PWM is working. 
     */
 
@@ -38,7 +38,10 @@ module PWM_generator(
             trigger_step <= 0;
             clk_high <= 0; 
         end else begin
-           if(hz_ctr < 5000000)begin
+          
+          if(hz_ctr<1250000)begin     //gen 40hz
+      // gen 20hz     if(hz_ctr < 2500000)begin
+           //   gen 10 hz if(hz_ctr < 5000000)begin
                hz_ctr <= hz_ctr + 1;
                trigger_step <= 0;
            end else begin
